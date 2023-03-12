3c23d15c48d1d873b01d8ffc2bf32cbfd954a392
ltugyi <tugyil@gmail.com>
2023-03-12 09:58:47 -0400

    Method not allowed is done

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -196,3 +196,8 @@ class TestAccountService(TestCase):
         """It should not delete an Account that is not found"""
         response = self.client.delete(f"{BASE_URL}/1")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
